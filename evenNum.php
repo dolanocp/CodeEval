bbca7f7c05986178f86ffb8695f223efdcad433e
Paul Dolan <dolanmail@gmail.com>
2014-06-06 15:32:08 +0100

    Update evenNum.php

--- evenNum.php
+++ evenNum.php
@@ -8,20 +8,17 @@
  * Write a program which checks input numbers and determines whether a number is even or not.
  */
 
-//text file containing: 701 4123 2936. These are the test values.
-$file = "testfile.txt";
-
 //gets the contents of the file as a string and then splits it using \n delimiter
-$contents = explode("\n",file_get_contents($file));
+$contents = explode("\n",file_get_contents($argv[1]));
 
 //iterate through each line in the file
 foreach($contents as $val){
     
     //use modulus to check if number is even
     if($val % 2){
-        echo "<p>0</p>";
+        echo "0\r";
     }
     else {
-        echo "<p>1</p>";
+        echo "\r1\r";
     }
 }
